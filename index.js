3f136e5625491d8c611ea5dc7ca636d006b6d3e9
JoelAntaribab <joelantaribaba@gmail.com>
2022-05-28 02:39:53 +0900

    Looping Data Ke-1

--- index.js
+++ index.js
@@ -11,6 +11,7 @@
     ...
   ]
 */
+
 const arrayObjectPegawai = require("./data-customer.json");
 
 function lakukanLooping(arrPegawai) {
@@ -18,7 +19,7 @@ function lakukanLooping(arrPegawai) {
   let dataYangAkanDilooping = arrPegawai;
 
   /*
-    TODO 1: Buatlah sebuah variabel bernama "hasilLooping" 
+    TODO 1: Buatlah sebuah variabel bernama "hasilLooping"
       yang berisi gabungan nama depan dan belakang dari masing masing pegawai
 
       Contoh: ["Aisyah Nirmala", "Mansur Faisal", ...]
@@ -49,6 +50,33 @@ function lakukanLooping(arrPegawai) {
         "Jumlah Pria dan Wanita berimbang"
   */
   let komentar = null;
+
+  let emptyData = [];
+  let jp = [];
+  let jw = [];
+
+  for (let i = 0; i < dataYangAkanDilooping.length; i++) {
+    emptyData.push(`${dataYangAkanDilooping[i].namaDepan} ${dataYangAkanDilooping[i].namaBelakang}`);
+    if (dataYangAkanDilooping[i].jenisKelamin === "M") {
+      jp.push(dataYangAkanDilooping[i]);
+    } else if (dataYangAkanDilooping[i].jenisKelamin === "F") {
+      jw.push(dataYangAkanDilooping[i]);
+    }
+  }
+
+  if (jp.length > jw.length) {
+    komentar = "Jumlah Pria lebih banyak dari Wanita";
+  }
+  if (jp.length < jw.length) {
+    komentar = "Jumlah Wanita lebih banyak dari Pria";
+  }
+  if (jp.length === jw.length) {
+    komentar = "Jumlah Pria dan Wanita berimbang";
+  }
+
+  hasilLooping = emptyData;
+  jumlahPria = jp.length;
+  jumlahWanita = jw.length;
 
   // ! JANGAN DIMODIFIKASI
   return {
